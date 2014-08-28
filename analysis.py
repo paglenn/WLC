25a2f96987c5ba77c1575e37678808ffb86d1253
paglenn <nls.pglenn@gmail.com>
2014-08-27 22:50:42 -0700

    updated analysis file to compute correlation function

--- analysis.py
+++ analysis.py
@@ -19,20 +19,66 @@ for line in infile.readlines():
     data = [float(x) for x in line2]
     trajectory[step].append(np.array(data))
 
-# compute angles
-cos = []
-for filament in trajectory:
-
-    for i in range(num_actins - 1) :
-
-        cos.append( np.dot(filament[i],filament[i+1]) )
-
+# compute P( cos \theta  )
 import matplotlib.pyplot as plt
-plt.hist(cos,bins=100,range=(-1,1),normed=True,histtype='step')
-x = np.linspace(-1,1,1000)
-P = 1./dx * np.exp(x/dx) / (2*np.sinh(1./dx) )
-plt.plot(x,P,'k',lw=1.5)
-plt.show()
+def plot_distribution():
+    cos = []
+    for filament in trajectory:
+
+        for i in range(num_actins - 1) :
+
+            cos.append( np.dot(filament[i],filament[i+1]) )
+
+    plt.hist(cos,bins=100,range=(-1,1),normed=True,histtype='step')
+    x = np.linspace(-1,1,1000)
+    P = 1./dx * np.exp(x/dx) / (2*np.sinh(1./dx) )
+    plt.plot(x,P,'k',lw=1.2)
+    plt.xlabel(r'$\cos \theta $')
+    plt.ylabel(r'$P(\cos \theta ) $')
+    plt.savefig('cos_hist.png')
+    plt.show()
+
+# compute correlator
+def plot_correlator():
+    R = range(0,30)
+    G = [0 for r in R ]
+    num_samples = list(G)
+    for filament in trajectory[1:]:
+
+        for r in R:
+
+            for i in range(num_actins - r):
+
+                num_samples[r] += 1
+                G[r] += np.dot(filament[i],filament[i+r])
+
+
+    for r in R:
+
+        G[r] = G[r] / num_samples[r]
+
+    # rescale R
+    R = [dx * r for r in R ]
+    R = np.array(R)
+    G = np.array(G)
+    #plt.plot(np.array(R),G,'b.')
+    plt.semilogy(np.array(R),G,'b.')
+
+    # model prediction: G(s) ~ exp(-s/l_p)
+    expR = np.exp(-R)
+    #plt.plot(R, expR,'k',lw=1.5 )
+    plt.semilogy(R, expR,'k',lw=1.5 )
+    plt.xlabel(r's/$l_p$')
+    plt.ylabel('G(s)')
+    #plt.savefig('correlator.png')
+    plt.savefig('correlator_log.png')
+    plt.show()
+
+plot_correlator()
+
+
+
+
 
 
 
