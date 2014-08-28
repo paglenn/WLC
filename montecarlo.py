e9c83ce804339f44859fc31025ef62eb08c99b87
paglenn <nls.pglenn@gmail.com>
2014-08-28 14:46:11 -0700

    minor changes to analysis file (more general)

--- montecarlo.py
+++ montecarlo.py
@@ -4,9 +4,10 @@ import numpy as np
 from actin import *
 
 # choose initial configuration to be along z-axis
+#dx = 0.002
 dx = 0.1
-L =  10
-MC_steps = int(1e5)
+L =  20
+nsteps = int(1e5)
 N = int(L/dx) + 1
 data_file = open('output.dat','w')
 
@@ -17,7 +18,7 @@ A = [ actin_filament(positions[i],dx) for i in range(N) ]
 # write initial config to file
 write(A,0,data_file)
 
-for j in range(1,MC_steps+1):
+for j in range(1,nsteps+1):
 
     random_vec = np.random.randn(3)
 
