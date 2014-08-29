182f3c8276c20246e8b6e4bd81bff90159506cd6
paglenn <nls.pglenn@gmail.com>
2014-08-29 10:53:49 -0700

    calculation of horizontal displacement vector

--- analysis.py
+++ analysis.py
@@ -1,10 +1,14 @@
 import numpy as np
-infile = open('output.dat','r')
+import os
+infile = 'output.dat'
+if not os.path.isfile(infile): exit('Data file missing!')
+infile = open(infile,'r')
 
-num_steps = int(1e5)
-dx = 0.5
-L = 500
-num_actins = int(L/dx)  + 1
+num_steps = int(1e6)
+dx =1
+L = 1000
+Ldx = int(L/dx)
+num_actins = Ldx + 1
 
 trajectory = [ list() for x in range(num_steps+1) ]
 step = -1
@@ -20,9 +24,28 @@ for line in infile.readlines():
     data = [float(x) for x in line2]
     trajectory[step].append(np.array(data))
 
+# calculate tip deviation
+filament = trajectory[-1]
+end_actin = filament[-1]
+T = [ filament[-1] for filament in trajectory ]
+
+def calculate_displacement():
+
+	original_filament = trajectory[0]
+	final_filament = trajectory[-1]
+	R = 0
+	for i in range(len(final_filament)):
+		scalar_projection = np.dot(final_filament[i],init_filament[i])/np.linalg.norm(init_filament[i])**2.
+		dR = final_filament[i] - scalar_projection*final_filament[i]
+		dR = dt / np.linalg.norm(dt)
+		R += dR * dx
+	return R
 
 def free_energy():
-    Ld = num_actins - 1
-    FE = - Ld * (1./dx) * np.log(np.sinh(1./dx) )
+    FE = - Ldx * (1./dx) * np.log(np.sinh(1./dx) )
     return FE
 
+R = calculate_displacement()
+print R
+F = free_energy()
+print F
