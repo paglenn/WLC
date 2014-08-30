9121a6191ffe5fc4047e5391fbfc35ba5f34650e
paglenn <nls.pglenn@gmail.com>
2014-08-30 03:12:07 -0700

    separate umbrella sampling output

--- umbrella_sampling/montecarlo.py
+++ umbrella_sampling/montecarlo.py
@@ -4,40 +4,28 @@ from actin import *
 from parameters import *
 
 # choose initial configuration to be along z-axis
-'''
-dx = 0.002
-L =  0.2
-numsteps = int(1e3)
-N = int(L/dx) + 1
-data_file = open('output.dat','w')
+if umbrella_bias:
+    out = dict()
+    for i in range(num_windows):
+        out[z_windows[i]] = open(window_files[i],'w')
+else:
+    outfile = open(data_file, 'w')
 
-# umbrella sampling windows
-num_windows = 10
-windows = [(i*L/num_windows,(1+i)*L/num_windows) for i in range(num_windows) ]
-
-'''
-outfile = open(data_file, 'w')
 N = num_actins
 positions = np.zeros((N,3))
 positions[:,2] = np.linspace(0,L,N)
 A0 = [ actin_filament(positions[i],dx) for i in range(N) ]
-# instead of rigid initial configuration, rotate all by their own angle
-'''
-for i in range(len(A)):
 
-    phi = np.random.uniform(0,2*np.pi)
-
-    A[i].rotate(phi)
-
-    update(A,i)
-'''
 # write initial configuration to file
-write_tangents(A,0,outfile)
+
+for dataFile in out.values():
+    write_tangents(A0,0,dataFile)
 
 
 for w in z_windows:
 
-    A = adjust_z(A0,A0,w)
+    A = list(A0)
+    A = adjust_z(A0,A,w)
 
     for j in range(1,numsteps+1):
 
@@ -60,9 +48,11 @@ for w in z_windows:
 
         update(A,random_index)
 
-        write_tangents(A,j,outfile)
+        write_tangents(A,j,out[w])
 
-outfile.close()
+for dataFile in out.values():
+    dataFile.write("program finished!!")
+    dataFile.close()
 
 
 
--- umbrella_sampling/parameters.py
+++ umbrella_sampling/parameters.py
@@ -1,4 +1,4 @@
-numsteps = int(1e5)
+numsteps = int(1e4)
 L = 0.2
 dx = 0.002
 Ldx = int(L/dx)
@@ -9,4 +9,5 @@ data_file = 'output.dat'
 umbrella_bias = True
 num_windows = 10
 z_windows = [(i*L/num_windows,(1+i)*L/num_windows) for i in range(num_windows) ]
+window_files = ["window_{0}".format(i) for i in range(num_windows) ]
 
