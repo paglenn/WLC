57559e91de77bd2e2fc6bbe6593297b13f585c3e
paglenn <nls.pglenn@gmail.com>
2014-09-17 11:44:22 -0700

    single run - two look Gaussian

--- single_run/engine.h
+++ single_run/engine.h
@@ -6,51 +6,73 @@
 #include<cstdlib>
 #include<random>
 #include<ctime>
+#include<TH1D.h>
+#include<TFile.h>
+#include<TCanvas.h>
+#include<TF1.h>
+#include<TPad.h>
 //using namespace std; 
 
 int j, step, w, wpass; 
 int seed = time(0); 
 std::default_random_engine rng (seed);
-const int numSteps = 1000000; 
+const int numSteps =1000000000; 
 double L = 0.1; 
 const int N = 100; 
 double delta = L/double(N); 
 double gauss_var = pow(1.5*L,2); 
 double tx[N],ty[N],tz[N]; 
 
-int numPasses = 500; 
-const int numWindows = 10; 
+int numPasses = 1; 
+const int numWindows = 1; 
 int numBins = 100; 
 double binWidth = 1./numBins; 
 double binOverlap = 5*binWidth; 
-double k = 1./delta; 
 int numFrames = numWindows * numPasses * numSteps;
 double winMin[numWindows],winMax[numWindows],zmin[numWindows];
+double k = 1./delta; 
 FILE * zFile;
 FILE * progressFile; 
 FILE * cosFile; 
 FILE * tpFile; 
 FILE * rpFile;
 FILE * rptpFile; 
+TFile DataFile("data.root","recreate");
+TH1D* rpHist = new TH1D(); 
+TH1D* tpHist = new TH1D(); 
+TH1D* rptpHist = new TH1D(); 
+TH1D* cosHist = new TH1D(); 
+const double PI = acos(-1.);
 
 
 double wmax,wmin,width, wmean, z ;
 double target, tol; 
 int random_index ; 
 std::vector<double> t0(3,0.),t_old(3,0.); 
-double kdE; 
 
 void init() {
 
 	t0.at(2) = 1.; 
 	zFile = fopen("uwham.dat","w");
-	rpFile = fopen("rpvals.dat","w");
-	tpFile = fopen("tpvals.dat","w");
-	rptpFile = fopen("rptpvals.dat","w");
+	//rpFile = fopen("rpvals.dat","w");
+	//tpFile = fopen("tpvals.dat","w");
+	//rptpFile = fopen("rptpvals.dat","w");
+	//cosFile = fopen("cos.dat","w"); 
+	rpHist->SetTitle("rp");
+	tpHist->SetTitle("tp");
+	rptpHist->SetTitle("<R_{#perp}, T_{#perp}>");
+	cosHist->SetTitle("cosines");
+	cosHist->SetBit(TH1::kCanRebin);
+	rpHist->SetBit(TH1::kCanRebin);
+	tpHist->SetBit(TH1::kCanRebin);
+	rptpHist->SetBit(TH1::kCanRebin);
+	cosHist->SetBins(100,0.9,1.0);
+	rpHist->SetBins(100,-0.01,0.01);
+	tpHist->SetBins(100,-0.01,0.01);
+	rptpHist->SetBins(100,-0.01,0.01);
 	srand(seed); 
 
 	progressFile = fopen("progress.out","w"); 
-	cosFile = fopen("cos.dat","w"); 
 
 	for(int j = 0; j < N; j++) {
 
@@ -99,7 +121,9 @@ void perturb(int index) {
 double dot(double x1, double y1, double z1, double x2, double y2, double z2) {
 
 	double sum = x1*x2 + y1*y2 + z1*z2; 
-	return sum ; 
+	double chk1 = sqrt(x1*x1+y1*y1+z1*z1); 
+	double chk2 = sqrt(x2*x2+y2*y2+z2*z2); 
+	return sum/(chk1*chk2) ; 
 }
 
 double getZ() {
@@ -109,8 +133,12 @@ double getZ() {
 }
 
 double getRP() {
-	double rp = 0.; 
-	for(int j = 0; j < N; j++) rp += delta*sqrt(tx[j]*tx[j]+ty[j]*ty[j]);
+	double rpx = 0.,rpy = 0.;
+	for(int j = 0; j < N; j++) {
+		rpx += tx[j]; 
+		rpy += ty[j];
+	}
+	double rp = delta*sqrt(rpx*rpx+rpy*rpy);
 	return rp/L;
 }
 
@@ -119,15 +147,18 @@ double getRPTP() {
 	double tpx=0.,tpy=0.;
 	tpx = tx[N-1]; tpy = ty[N-1]; 
 	for(int i = 1; i < N; i++) {
-		rx += delta*tx[i];
-		ry += delta*ty[i];
+		rx += tx[i];
+		ry += ty[i];
 	}
 
-	return (rx*tpx + ry*tpy)/float(L); 
+	return (rx*tpx + ry*tpy); 
 }
 
 	
-double getTP() { return sqrt(tx[N-1]*tx[N-1] + ty[N-1]*tx[N-1]); }
+double getTP() { 
+	double tp = sqrt(tx[N-1]*tx[N-1] + ty[N-1]*ty[N-1]); 
+	return tp; 
+}
 
 void check() {
 
@@ -153,10 +184,10 @@ double deltaE(int index, int w_index) {
 	
 	double dE = (E_new - E_old) ; 
 
-	z = getZ(); 
+	//z = getZ(); 
 	//dE += 0.5*k*pow(z - zmin[w_index],2.) ; 
 
-	return k*dE;
+	return dE;
 
 }
 
@@ -187,74 +218,113 @@ void adjustZ(int w_index) {
 		double z_new = getZ(); 
 		double dz = z_new - z; 
 
-		if(z > target && dz > 0 ) dz = revert(random_index); 
-		if(z < target && dz < 0 ) dz = revert(random_index); 
+		if(z > target & dz > 0 ) dz = revert(random_index); 
+		if(z < target & dz < 0 ) dz = revert(random_index); 
 
 		if(dz!=0) z = z_new; 
 	}
 }
 
 
-bool umbrella_mc_step(int w_index) {
+
+void write_cosines() {
+
+	for(int j =0; j< N-1; j++) {
+		double tcos = dot(tx[j],ty[j],tz[j],tx[j+1],ty[j+1],tz[j+1]);  
+		/*
+		char cosString[50]; 
+		sprintf(cosString,"%f\n",cos);
+		fputs(cosString,cosFile); 
+		*/
+		cosHist->Fill(tcos); 
+	}
+}
+
+void write_TP() {
+
+	double tp = getTP(); 
+	/*
+	char tpString[50]; 
+	sprintf(tpString,"%f\n",tp);
+	fputs(tpString,tpFile); 
+	*/
+	tpHist->Fill(tp);
+}
+
+void write_RP() {
+
+	double rp = getRP(); 
+	/*
+	char rpString[50]; 
+	sprintf(rpString,"%f\n",rp);
+	fputs(rpString,rpFile); 
+	*/
+	rpHist->Fill(rp);
+}
+
+void write_RPTP() {
+
+	double rptp = getRPTP(); 
+	/*
+	char rptpString[50]; 
+	sprintf(rptpString,"%f\n",rptp);
+	fputs(rptpString,rptpFile); 
+	*/
+	rptpHist->Fill(rptp);
+}
+
+bool umbrella_mc_step(int w_index = numWindows-1) {
 
 	//std::cout<<tx[1]<<ty[1]<<tz[1]<<std::endl;
 	random_index = 1 + rand() % (N-1); // random int (1,N-1)  
 
 	perturb(random_index); 
 
-	kdE = deltaE(random_index, w_index); 
+	double dE = deltaE(random_index, w_index); 
 	//std::cout<< kdE << std::endl;
 
-	if(kdE <= 0.) return true; 
-	else if (rand()/double(RAND_MAX) > exp(-kdE) ) {
+	if(dE <= 0.) return true; 
+	else if (rand()/double(RAND_MAX) > exp(-dE/delta) ) {
 
 		revert(random_index); 
 		return false;
 	}
+	
 
 	return true; 
 
 }
 
-void write_cosines() {
+void WriteEventData() {
 
-	for(int j =0; j< N-1; j++) {
-		double cos = dot(tx[j],ty[j],tz[j],tx[j+1],ty[j+1],tz[j+1]);  
-		char cosString[50]; 
-		sprintf(cosString,"%f\n",cos);
-		fputs(cosString,cosFile); 
-	}
+	write_cosines();
+	write_RP();
+	write_TP();
+	write_RPTP();
 }
 
-void write_TP() {
-
-	for(int j =0; j< N-1; j++) {
-		double tp = getTP(); 
-		char tpString[50]; 
-		sprintf(tpString,"%f\n",tp);
-		fputs(tpString,tpFile); 
-	}
+void writeHistograms() {
+	// normalize all 
+	rpHist -> Scale(1./rpHist->Integral("width")) ; 
+	tpHist -> Scale(1./tpHist->Integral("width")) ; 
+	rptpHist -> Scale(1./rptpHist->Integral("width") ) ; 
+	cosHist -> Scale(1./cosHist->Integral("width")) ; 
+	
+	rpHist->Write("rp");
+	rptpHist->Write("rptp");
+	tpHist->Write("tp");
+	cosHist->Write("dcos");
 }
 
-void write_RP() {
-
-	for(int j =0; j< N-1; j++) {
-		double rp = getRP(); 
-		char rpString[50]; 
-		sprintf(rpString,"%f\n",rp);
-		fputs(rpString,rpFile); 
-	}
+int cleanup() {
+	fclose(zFile); 
+	fclose(progressFile);
+	//fclose(cosFile);
+	//DataFile.Write(); 
+	DataFile.Close();
+	return 0 ; 
 }
 
-void write_RPTP() {
-
-	for(int j =0; j< N-1; j++) {
-		double rptp = getRPTP(); 
-		char rptpString[50]; 
-		sprintf(rptpString,"%f\n",rptp);
-		fputs(rptpString,rptpFile); 
-	}
-}
 
 
 
--- single_run/simulation.cpp
+++ single_run/simulation.cpp
@@ -1,22 +1,33 @@
 #include "engine.h"
 #include<vector>
+#include<ctime>
 
 int main() {
 
+	int start_time = time(0); 
 	init(); 
 	int num_acc = 0; 
 	
 	for(int j = 0; j < numSteps; j++) { 
-		int acc = umbrella_mc_step(numWindows-1); 
-		if(acc) num_acc += 1; 
-		write_cosines();
-		write_RP();
-		write_TP();
-		write_RPTP();
-	}
-	std::cout<<100*num_acc/float(numSteps)<< "%"<<std::endl;
+		int acc = umbrella_mc_step(); 
+		num_acc += acc; 
+		//std::cout<<acc<<std::endl;
+		WriteEventData();	
+		// Write progress report 
+		char progress[50]; 
+		sprintf(progress,"step %d/%d\n",j,numSteps);
+		fputs(progress,progressFile);
+	} // end simulation 
 
-	return 0; 
+	// write statistics and close up 
+	writeHistograms();
+	char ratio[50];
+	int end_time = time(0); 
+	double tdiff = (end_time - start_time)/float(60); // in minutes  
+	//sprintf(ratio,"%.1f%% steps accepted\n",100*num_acc/float(numFrames));
+	sprintf(ratio,"%.1f%% steps accepted\nrunning time: %.2f minutes\n",100*(num_acc/float(numSteps)),tdiff);
+	fputs(ratio,progressFile); 
+	return cleanup(); 
 }
 
 
